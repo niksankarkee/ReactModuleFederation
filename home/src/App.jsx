544ecf8b2a84e75820ec2dcb6b4b9ffec251b441
niksankarkee <karkeeniksan@gmail.com>
2022-04-23 16:16:35 +0900

    error handling

--- home/src/App.jsx
+++ home/src/App.jsx
@@ -7,7 +7,7 @@ import './index.scss';
 
 const App = () => (
   <div className='text-3xl mx-auto max-w-6xl'>
-    <Header />
+    <Header app={{ name: 'home' }} />
     <div className='my-10'>Home Page Content</div>
     <Footer />
   </div>
--- pdp/src/App.jsx
+++ pdp/src/App.jsx
@@ -1,25 +1,18 @@
-import React, { Suspense, useState } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 
 import './index.scss';
-const Header = React.lazy(() => import('home/Header'));
 import Footer from 'home/Footer';
+import Header from 'home/Header';
+import SafeComponent from './SafeComponent';
 
-const App = () => {
-  const [showHeader, setShowHeader] = useState(false);
-  return (
-    <div className='text-3xl mx-auto max-w-6xl'>
-      {showHeader && (
-        <Suspense fallback={<div>Loading</div>}>
-          <Header />
-        </Suspense>
-      )}
-      <button className='text-3xl  p-5' onClick={() => setShowHeader(true)}>
-        Show Header
-      </button>
-      <div className='my-10'>Pdp Page Content</div>
-      <Footer />
-    </div>
-  );
-};
+const App = () => (
+  <div className='mt-10 text-3xl mx-auto max-w-6xl'>
+    <SafeComponent>
+      <Header />
+    </SafeComponent>
+    <div className='my-10'>Pdp Page Content</div>
+    <Footer />
+  </div>
+);
 ReactDOM.render(<App />, document.getElementById('app'));
