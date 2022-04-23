fedc1f67c1708fdd5c058319d52198d8871d8d30
niksankarkee <karkeeniksan@gmail.com>
2022-04-23 15:56:54 +0900

    module federation with async

--- pdp/src/App.jsx
+++ pdp/src/App.jsx
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { Suspense, useState } from 'react';
 import ReactDOM from 'react-dom';
 
 import './index.scss';
+const Header = React.lazy(() => import('home/Header'));
 import Footer from 'home/Footer';
-import Header from 'home/Header';
 
-const App = () => (
-  <div className='mt-10 text-3xl mx-auto max-w-6xl'>
-    <Header />
-    <div className='my-10'>Pdp Page Content</div>
-    <Footer />
-  </div>
-);
+const App = () => {
+  const [showHeader, setShowHeader] = useState(false);
+  return (
+    <div className='text-3xl mx-auto max-w-6xl'>
+      {showHeader && (
+        <Suspense fallback={<div>Loading</div>}>
+          <Header />
+        </Suspense>
+      )}
+      <button className='text-3xl  p-5' onClick={() => setShowHeader(true)}>
+        Show Header
+      </button>
+      <div className='my-10'>Pdp Page Content</div>
+      <Footer />
+    </div>
+  );
+};
 ReactDOM.render(<App />, document.getElementById('app'));
